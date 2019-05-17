27c22022d2c7f18c1e71caaef44ce9a114c19bb8
KevinAGV <47923484+KevinAGV@users.noreply.github.com>
2019-05-16 22:37:53 -0500

    Se agrego código para subir un archivo a FTP

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace FTPsubir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bsubir_Click(object sender, EventArgs e)
+        {
+           
+
+            FtpWebRequest solicitud = (FtpWebRequest)FtpWebRequest.Create("ftp://files.000webhost.com/public_html/" + "/" + Path.GetFileName( "C:\\Users\\ALBERTO\\Desktop\\a20146129.txt"));
+            solicitud.Method = WebRequestMethods.Ftp.UploadFile;
+            solicitud.Credentials = new NetworkCredential("test1pweb", "computadora321");
+            solicitud.UsePassive = true;
+            solicitud.UseBinary = true;
+            solicitud.KeepAlive = false;
+
+
+            FileStream abrir= File.OpenRead("C:\\Users\\ALBERTO\\Desktop\\a20146129.txt");
+            byte[] buffer = new byte[abrir.Length];
+            abrir.Read(buffer, 0, buffer.Length);
+            abrir.Close();
+
+
+            Stream streamQuery = solicitud.GetRequestStream();
+            streamQuery.Write(buffer, 0, buffer.Length);
+            streamQuery.Close();
+            
+        }
+    }
+}
